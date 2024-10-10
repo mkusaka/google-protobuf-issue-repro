f04680914243972011f126bb9191e90bfd910f25
mkusaka <hinoshita1992@gmail.com>
2024-10-10 19:02:41 +0900

    init

---
+++ v4/main.rb
@@ -0,0 +1,48 @@
+require 'google/protobuf'
+
+require_relative './addressbook_pb'
+
+person = Tutorial::Person.new(phone_numbers: ["123-456-7890", "987-654-3210"])
+
+puts "person#to_h"
+puts person.to_h
+puts "persion#phone_numbers"
+puts person.phone_numbers
+
+number_empty_person = Tutorial::Person.new(phone_numbers: [])
+
+puts "number_empty_person#to_h"
+puts number_empty_person.to_h
+puts "number_empty_person#phone_numbers"
+puts number_empty_person.phone_numbers.inspect
+
+number_default_person = Tutorial::Person.new
+
+puts "number_default_person#to_h"
+puts number_default_person.to_h
+puts "number_default_person#phone_numbers"
+puts number_default_person.phone_numbers.inspect
+require 'google/protobuf'
+
+require_relative './addressbook_pb'
+
+person = Tutorial::Person.new(phone_numbers: ["123-456-7890", "987-654-3210"])
+
+puts "person#to_h"
+puts person.to_h
+puts "persion#phone_numbers"
+puts person.phone_numbers
+
+number_empty_person = Tutorial::Person.new(phone_numbers: [])
+
+puts "number_empty_person#to_h"
+puts number_empty_person.to_h
+puts "number_empty_person#phone_numbers"
+puts number_empty_person.phone_numbers.inspect
+
+number_default_person = Tutorial::Person.new
+
+puts "number_default_person#to_h"
+puts number_default_person.to_h
+puts "number_default_person#phone_numbers"
+puts number_default_person.phone_numbers.inspect
