a9e5376eb95e2f9a2861f2e4bfb6f9fb7b440d1d
mkusaka <hinoshita1992@gmail.com>
2024-10-21 13:41:42 +0900

    tidy up

--- go/main.go
+++ go/main.go
@@ -1,11 +1,11 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"log"
 
 	tutorial "github.com/mkusaka/google-protobuf-issue-repro/go/generated"
+	"google.golang.org/protobuf/encoding/protojson"
 )
 
 func main() {
@@ -18,7 +18,11 @@ func main() {
 	}
 
 	// JSON 形式に marshal した結果を確認
-	jsonData, err := json.Marshal(person)
+	option := protojson.MarshalOptions{
+		EmitUnpopulated: true,
+	}
+
+	jsonData, err := option.Marshal(person)
 	if err != nil {
 		log.Fatalf("JSON marshaling failed: %s", err)
 	}
@@ -26,43 +30,43 @@ func main() {
 	fmt.Println("person JSON marshal の結果:")
 	fmt.Println(string(jsonData))
 
-	// 各フィールドを確認
-	fmt.Println("person#phone_numbers の type:")
-	if len(person.PhoneNumbers) > 1 {
-		fmt.Println(person.PhoneNumbers[1].GetType()) // Type のデフォルト値が何か確認
-	}
-
-	// 空の phone_numbers を持つ Person メッセージ
-	numberEmptyPerson := &tutorial.Person{
-		PhoneNumbers: []*tutorial.PhoneNumber{}, // 空の phone_numbers
-	}
-
-	// JSON 形式に marshal した結果を確認
-	jsonDataEmpty, err := json.Marshal(numberEmptyPerson)
-	if err != nil {
-		log.Fatalf("JSON marshaling failed: %s", err)
-	}
-
-	fmt.Println("number_empty_person JSON marshal の結果:")
-	fmt.Println(string(jsonDataEmpty))
-
-	// 各フィールドを確認
-	fmt.Println("number_empty_person#phone_numbers:")
-	fmt.Println(numberEmptyPerson.PhoneNumbers)
-
-	// デフォルトの Person メッセージ（PhoneNumbers フィールド未設定）
-	numberDefaultPerson := &tutorial.Person{}
-
-	// JSON 形式に marshal した結果を確認
-	jsonDataDefault, err := json.Marshal(numberDefaultPerson)
-	if err != nil {
-		log.Fatalf("JSON marshaling failed: %s", err)
-	}
-
-	fmt.Println("number_default_person JSON marshal の結果:")
-	fmt.Println(string(jsonDataDefault))
-
-	// 各フィールドを確認
-	fmt.Println("number_default_person#phone_numbers:")
-	fmt.Println(numberDefaultPerson.PhoneNumbers) // nil または未設定
+	//// 各フィールドを確認
+	//fmt.Println("person#phone_numbers の type:")
+	//if len(person.PhoneNumbers) > 1 {
+	//	fmt.Println(person.PhoneNumbers[1].GetType()) // Type のデフォルト値が何か確認
+	//}
+	//
+	//// 空の phone_numbers を持つ Person メッセージ
+	//numberEmptyPerson := &tutorial.Person{
+	//	PhoneNumbers: []*tutorial.PhoneNumber{}, // 空の phone_numbers
+	//}
+	//
+	//// JSON 形式に marshal した結果を確認
+	//jsonDataEmpty, err := json.Marshal(numberEmptyPerson)
+	//if err != nil {
+	//	log.Fatalf("JSON marshaling failed: %s", err)
+	//}
+	//
+	//fmt.Println("number_empty_person JSON marshal の結果:")
+	//fmt.Println(string(jsonDataEmpty))
+	//
+	//// 各フィールドを確認
+	//fmt.Println("number_empty_person#phone_numbers:")
+	//fmt.Println(numberEmptyPerson.PhoneNumbers)
+	//
+	//// デフォルトの Person メッセージ（PhoneNumbers フィールド未設定）
+	//numberDefaultPerson := &tutorial.Person{}
+	//
+	//// JSON 形式に marshal した結果を確認
+	//jsonDataDefault, err := json.Marshal(numberDefaultPerson)
+	//if err != nil {
+	//	log.Fatalf("JSON marshaling failed: %s", err)
+	//}
+	//
+	//fmt.Println("number_default_person JSON marshal の結果:")
+	//fmt.Println(string(jsonDataDefault))
+	//
+	//// 各フィールドを確認
+	//fmt.Println("number_default_person#phone_numbers:")
+	//fmt.Println(numberDefaultPerson.PhoneNumbers) // nil または未設定
 }
